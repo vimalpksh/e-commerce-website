390fa5d992b84ce12340b36711df766bacd1735d
vimalpksh <itisvimal@gmail.com>
2024-05-03 22:57:52 +0530

    Payment and footer section done
    working on responsiveness of the site

--- script.js
+++ script.js
@@ -2,8 +2,142 @@ const wrapper = document.querySelector(".sliderWrapper");
 
 const menuItems = document.querySelectorAll(".menuItem");
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 2300,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 3000,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 2300,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 3400,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 2600,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currProductImage = document.querySelector(".productImg");
+const currProductTitle = document.querySelector(".productTitle");
+const currProductPrice = document.querySelector(".productPrice");
+const currProductColors = document.querySelectorAll(".color");
+const currProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    // change current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    // Change the choosen product
+
+    choosenProduct = products[index];
+
+    // Change texts of current product
+
+    currProductTitle.textContent = choosenProduct.title;
+    currProductPrice.textContent = "Rs." + choosenProduct.price;
+    currProductImage.src = choosenProduct.colors[0].img;
+
+    //Changing color
+
+    currProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
   });
+});
+
+currProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currProductImage.src = choosenProduct.colors[index].img;
+  });
+});
+
+currProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+// ======PAYMENT MODAL========
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
 });
